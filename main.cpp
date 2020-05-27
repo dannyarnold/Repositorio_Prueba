188a805aa693dc2014e52013a987fa1bf822e890
danny <yo@email.com>
2020-05-27 00:37:34 -0300

    Commit en nueva rama

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> Obtener_Primos(int limit)
+{
+    int divisiones = 0;
+    vector<int> resultados;
+
+    for (int i = 1; i < limit; i++)
+    {
+        divisiones = 0;
+        for (int j = 1; j < limit; j++)
+        {
+            if (i % j == 0)
+            {
+                divisiones++;
+            }
+        }
+        if (divisiones == 2)
+        {
+            resultados.push_back(i);
+        }
+    }
+    return resultados;
+}
+
+int main()
+{
+    for (int primo : Obtener_Primos(100))
+    {
+        cout << primo << endl;
+    }
+}
